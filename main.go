4412364c602656cfab8e677782b7aacb581abd3a
wjdqhry <yung12320@daum.net>
2021-03-27 00:37:59 +0900

    fix des folder

--- main.go
+++ main.go
@@ -13,16 +13,16 @@ func main() {
 	startTime := time.Now()
 
 	cpFolders := map[string]string{
-		"/data/sftp/1차_구축사업/unit5/07랜드마크_이미지_AI데이터":      "/data/ai_data_2020_01",
-		"/data/sftp/1차_구축사업/unit5/15한국인_재식별_이미지":         "/data/ai_data_2020_01",
-		"/data/sftp/1차_구축사업/unit5/19위성영상_객체판독":           "/data/ai_data_2020_01",
-		"/data/sftp/1차_구축사업/unit1/10사람_인체자세_3D_AI데이터":    "/data/ai_data_2020_01",
-		"/data/sftp/1차_구축사업/unit1/13피트니스_자세_이미지":         "/data/ai_data_2020_01",
-		"/data/sftp/1차_구축사업/unit2/09한국인_대화음성_AI데이터":      "/data/ai_data_2020_01",
-		"/data/sftp/1차_구축사업/unit2/18감성_대화_말뭉치":           "/data/ai_data_2020_01",
-		"/data/sftp/1차_구축사업/unit3/12도로환경_파노라마_이미지_AI데이터": "/data/ai_data_2020_01",
-		"/data/sftp/1차_구축사업/unit3/16도로주행영상":              "/data/ai_data_2020_01",
-		"/data/sftp/1차_구축사업/unit5/03딥페이크_변조영상_AI데이터":     "/data/ai_data_2020_01",
+		"/data/sftp/1차_구축사업/unit5/07랜드마크_이미지_AI데이터":      "/data/ai_data_2020_01/07",
+		"/data/sftp/1차_구축사업/unit5/15한국인_재식별_이미지":         "/data/ai_data_2020_01/15",
+		"/data/sftp/1차_구축사업/unit5/19위성영상_객체판독":           "/data/ai_data_2020_01/19",
+		"/data/sftp/1차_구축사업/unit1/10사람_인체자세_3D_AI데이터":    "/data/ai_data_2020_01/10",
+		"/data/sftp/1차_구축사업/unit1/13피트니스_자세_이미지":         "/data/ai_data_2020_01/13",
+		"/data/sftp/1차_구축사업/unit2/09한국인_대화음성_AI데이터":      "/data/ai_data_2020_01/09",
+		"/data/sftp/1차_구축사업/unit2/18감성_대화_말뭉치":           "/data/ai_data_2020_01/18",
+		"/data/sftp/1차_구축사업/unit3/12도로환경_파노라마_이미지_AI데이터": "/data/ai_data_2020_01/12",
+		"/data/sftp/1차_구축사업/unit3/16도로주행영상":              "/data/ai_data_2020_01/16",
+		"/data/sftp/1차_구축사업/unit5/03딥페이크_변조영상_AI데이터":     "/data/ai_data_2020_01/03",
 	}
 	for origin := range cpFolders {
 		if _, err := os.Stat(origin); err != nil {
